f9831b82bc6c1da515ecaf8c73a9b15c27d44190
Leonardo Arauo <leonardo.araujornb@gmail.com>
2016-12-08 14:26:52 -0300

    Rename questao011.c to questao11.c

--- ListaDeExercicios01-LP1-16.2/questao11.c
+++ ListaDeExercicios01-LP1-16.2/questao11.c
@@ -1,13 +1,13 @@
 /*
-UNIVERSIDADE ESTADUAL DA PARAÕBA
-Curso: CiÍncia da ComputaÁ„o
-Disciplina: Linguagem de ProgramaÁ„o 1
+UNIVERSIDADE ESTADUAL DA PARA√çBA
+Curso: Ci√™ncia da Computa√ß√£o
+Disciplina: Linguagem de Programa√ß√£o 1
 Professor: Danilo Abreu Santos
 Aluno: Leonardo Araujo Silva
 
-Lista 01: Entrada/SaÌda, vari·veis e expressıes
+Lista 01: Entrada/Sa√≠da, vari√°veis e express√µes
 
-11. (URI-1002) A formula para calcular a area de uma circunferencia e: area = PÃ * raio≤.
+11. (URI-1002) A formula para calcular a area de uma circunferencia e: area = P√å * raio¬≤.
 Considerando para este problema que PI = 3.14159. Efetue o calculo da area,
 elevando o valor de Raio ao quadrado e multiplicando por PI.
 Entrada: A entrada contem um valor de ponto flutuante (dupla precisao), no caso,
@@ -25,7 +25,7 @@ main(){
 	setlocale(LC_ALL, "Portuguese");
 	float raio, area;
 	
-	puts("Informe o valor do raio para calcular a ·rea : ");
+	puts("Informe o valor do raio para calcular a √°rea : ");
 	scanf("%f", &raio);
 	area = pow(raio, 2)*PI;
 	printf("A = %f\n", area);
